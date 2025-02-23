cf8be2a026f7e24d57f558aa8cdceb1835e5f6f1
Vanamu-Sumalatha <sumalathavanamu@gmail.com>
2025-02-23 11:58:05 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { Container } from "react-bootstrap";
 const Footer = () => {
   return (
     <Container fluid className="text-center py-3" style={{ backgroundColor: "#5d5c5c", color: "white" }}>
-      <p className="mb-0">© 2024 Vanamu Sumalatha. All rights reserved.</p>
+      <p className="mb-0">© 2025 Vanamu Sumalatha. All rights reserved.</p>
     </Container>
   );
 };
