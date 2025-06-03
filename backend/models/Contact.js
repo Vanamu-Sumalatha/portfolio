cbcf103aa0a42cfdc46b6ff70f9e2c166c9f42d6
Vanamu-Sumalatha <sumalathavanamu@gmail.com>
2025-06-03 19:16:26 +0530

    Update Contact.js

--- backend/models/Contact.js
+++ backend/models/Contact.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const contactSchema = new mongoose.Schema({
-    name: String,
-    phone: String,
-    email: String,
-    message: String,
-    createdAt: { type: Date, default: Date.now }
+  name: String,
+  phone: String,
+  email: String,
+  message: String,
+  createdAt: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('Contact', contactSchema);
+module.exports = mongoose.model("Contact", contactSchema);
